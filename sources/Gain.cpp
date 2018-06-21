c02a471d8810a89d3db7de5aafaa94177ad85ef9
Davide Busacca <38257285+DavideBusacca@users.noreply.github.com>
2018-06-21 11:00:20 +0200

    Add license notice

--- sources/Gain.cpp
+++ sources/Gain.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2017  Busacca Davide
+ *
+ * This file is part of DSP-Synthesis-Library
+ *
+ * DSP-Synthesis-Library is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * DSP-Synthesis-Library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with DSP-Synthesis-Library.  If not, see http://www.gnu.org/licenses/
+ */
+
 #ifndef GAIN_BUSACCA_CPP
 #define GAIN_BUSACCA_CPP
 
